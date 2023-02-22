264326529f8e488dc1e768f8085489275b8d0a6d
Alexei Prokudin <prokudin@jlab.org>
2023-02-22 15:25:20 -0500

    ps3_hangman.py is changed

--- Week3/ps3_hangman.py
+++ Week3/ps3_hangman.py
@@ -6,7 +6,7 @@
 # You don't need to understand this helper code,
 # but you will have to know how to use the functions
 # (so be sure to read the docstrings!)
-
+ 
 import random
 
 WORDLIST_FILENAME = "words.txt"
